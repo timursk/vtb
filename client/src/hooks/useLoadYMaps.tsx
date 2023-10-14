d9047d73d7f47691d177807eae8d5a985e23fb02
Timur Salikhov <timur.dev@mail.ru>
2023-10-14 13:27:01 +0300

    feat: add Cluster component, create ChangeCenter function for changing map location

--- client/src/hooks/useLoadYMaps.tsx
+++ client/src/hooks/useLoadYMaps.tsx
@@ -1,6 +1,7 @@
 import { Marker } from '@/components/Marker';
-import { LngLat } from '@yandex/ymaps3-types';
-import React from 'react';
+import { LngLat, YMapLocationRequest } from '@yandex/ymaps3-types';
+import { Feature } from '@yandex/ymaps3-types/packages/clusterer';
+import React, { useCallback } from 'react';
 import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
@@ -9,54 +10,106 @@ interface Props {
 }
 
 export function useLoadYMaps({ coordinates }: Props) {
+    const [location, setLocation] = useState<YMapLocationRequest>({
+        center: [37.623082, 55.75254],
+        zoom: 10,
+        duration: 1000,
+    });
+
+    // перемещения центра карты
+    const changeCenter = useCallback(
+        (newLocation: LngLat) => {
+            setLocation((prev) => ({
+                ...prev,
+                center: newLocation,
+            }));
+        },
+        [setLocation]
+    );
+
     const [YMaps, setYMaps] = useState(<div />);
     const map = useRef(null);
 
     useEffect(() => {
         (async () => {
             try {
+                // инициализация модулей
                 const [ymaps3React] = await Promise.all([
                     ymaps3.import('@yandex/ymaps3-reactify'),
                     ymaps3.ready,
                 ]);
                 const reactify = ymaps3React.reactify.bindTo(React, ReactDOM);
 
+                // получение компонентов
                 const {
                     YMap,
                     YMapDefaultSchemeLayer,
                     YMapDefaultFeaturesLayer,
                     YMapMarker,
-                    YMapControl,
+                    YMapControls,
                 } = reactify.module(ymaps3);
 
+                // инициализация модулей
+                const { YMapZoomControl, YMapGeolocationControl } =
+                    reactify.module(
+                        await ymaps3.import('@yandex/ymaps3-controls@0.0.1')
+                    );
+
+                const { YMapClusterer, clusterByGrid } = reactify.module(
+                    await ymaps3.import('@yandex/ymaps3-clusterer@0.0.1')
+                );
+
+                // создание маркера
+                const marker = (feature: Feature) => (
+                    <YMapMarker coordinates={feature.geometry.coordinates}>
+                        <Marker />
+                    </YMapMarker>
+                );
+
+                // создание кластера
+                const cluster = (coordinates: LngLat, features: Feature[]) => (
+                    <YMapMarker coordinates={coordinates}>
+                        <Marker />
+                    </YMapMarker>
+                );
+
+                const points = coordinates.map((lnglat, i) => ({
+                    type: 'Feature',
+                    id: i,
+                    geometry: { coordinates: lnglat },
+                    properties: { name: 'Point of issue of orders' },
+                })) as any;
+
+                // создание компонента карты
                 setYMaps(() => (
                     <YMap
-                        location={{
-                            center: [37.623082, 55.75254],
-                            zoom: 9,
-                        }}
+                        location={location}
                         camera={{ tilt: 0, azimuth: 0, duration: 0 }}
                         ref={map}
                     >
                         <YMapDefaultSchemeLayer />
                         <YMapDefaultFeaturesLayer />
 
-                        <YMapControl></YMapControl>
+                        <YMapControls position='right'>
+                            <YMapZoomControl />
+                            <YMapGeolocationControl />
+                        </YMapControls>
 
-                        {coordinates.map((lnglat, idx) => (
-                            <YMapMarker key={idx} coordinates={lnglat}>
-                                <Marker />
-                            </YMapMarker>
-                        ))}
+                        <YMapClusterer
+                            features={points}
+                            method={clusterByGrid({ gridSize: 64 })}
+                            marker={marker}
+                            cluster={cluster}
+                        />
                     </YMap>
                 ));
             } catch (e) {
                 setYMaps(<div />);
             }
         })();
-    }, [coordinates]);
+    }, [location, coordinates]);
 
-    return { YMaps, map };
+    return { YMaps, map, changeCenter };
 }
 
 // useEffect(() => {
@@ -126,41 +179,41 @@ export function useLoadYMaps({ coordinates }: Props) {
 //         const { YMapDefaultMarker } = await ymaps3.import(
 //             '@yandex/ymaps3-markers@0.0.1'
 //         );
-//         const { YMapClusterer, clusterByGrid } = await ymaps3.import(
-//             '@yandex/ymaps3-clusterer@0.0.1'
-//         );
+// const { YMapClusterer, clusterByGrid } = await ymaps3.import(
+//     '@yandex/ymaps3-clusterer@0.0.1'
+// );
 
-//         const marker = (feature) =>
-//             new YMapDefaultMarker({
-//                 coordinates: feature.geometry.coordinates,
-//                 title: '123',
-//                 subtitle: 'qwerty',
-//                 color: 'blue',
-//             });
+// const marker = (feature) =>
+//     new YMapDefaultMarker({
+//         coordinates: feature.geometry.coordinates,
+//         title: '123',
+//         subtitle: 'qwerty',
+//         color: 'blue',
+//     });
 
-//         const cluster = (coordinates, features) =>
-//             new YMapDefaultMarker({
-//                 coordinates,
-//                 title: '123',
-//                 subtitle: 'qwerty',
-//                 color: 'blue',
-//             });
+// const cluster = (coordinates, features) =>
+//     new YMapDefaultMarker({
+//         coordinates,
+//         title: '123',
+//         subtitle: 'qwerty',
+//         color: 'blue',
+//     });
 
-//         const points = coordinates.map((lnglat, i) => ({
-//             type: 'Feature',
-//             id: i,
-//             geometry: { coordinates: lnglat },
-//             properties: { name: 'Point of issue of orders' },
-//         })) as any;
+// const points = coordinates.map((lnglat, i) => ({
+//     type: 'Feature',
+//     id: i,
+//     geometry: { coordinates: lnglat },
+//     properties: { name: 'Point of issue of orders' },
+// })) as any;
 
-//         const clusterer = new YMapClusterer({
-//             method: clusterByGrid({ gridSize: 64 }),
-//             features: points,
-//             marker,
-//             cluster,
-//         });
+// const clusterer = new YMapClusterer({
+//     method: clusterByGrid({ gridSize: 64 }),
+//     features: points,
+//     marker,
+//     cluster,
+// });
 
-//         map.addChild(clusterer);
+// map.addChild(clusterer);
 
 //         // map.addChild(
 //         //     new YMapDefaultMarker({
